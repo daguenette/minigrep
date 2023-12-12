78680f2e1a49edd4e5df9a6d8219c85e59415a58
daguenette <daguenette.data@gmail.com>
2023-12-11 20:31:00 -0500

    Added small library logic

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+mod libs;
+
+use std::env;
+use std::process;
+use libs::Config;
+
 fn main() {
-    println!("Hello, world!");
+
+    // GET ARGS
+    let args: Vec<String> = env::args().collect();
+    let config: Config = Config::build(&args).unwrap_or_else(|err| {
+       println!("Problem parsing arguments: {err}");
+       process::exit(1);
+    });
+
+    // Run.
+    if let Err(e) = libs::run(config) {
+        println!("Application error: {e}");
+        process::exit(1);
+    }
+
 }
